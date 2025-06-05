75c11f6b85a3c0ba6b2964df877bf3ffc1ec68ce
Shiv Jagath <itsshivjagath@gmail.com>
2025-06-05 07:38:22 +0200

    React install/setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// components
+import RainbowLine from './components/RainbowLine';
+
+// css
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <RainbowLine />
+      <div>Hello world</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/RainbowLine.jsx
@@ -0,0 +1,7 @@
+import '../styles/RainbowLine.css';
+
+function RainbowLine() {
+  return <div className="rainbow-line"></div>;
+}
+
+export default RainbowLine;
